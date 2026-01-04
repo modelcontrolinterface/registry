0dce6286bba16f33753056f063caf33bd294fe1e
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2026-01-04 21:00:59 +0000

    chore: Reorder regex definitions in src/lib/regex.ts
    Reordered the `authorRegex` and `semanticVersionRegex` definitions within `src/lib/regex.ts` for better organization. No functional changes were made.

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -1,7 +1,6 @@
 export const packageNameRegex = /^[a-z](?:[a-z0-9_-]{0,62}[a-z0-9])?$/;
 
+export const authorRegex = /^([^<(]+?)(?:\s*<([^>]+)>)?(?:\s*\(([^)]+)\))?$/;
+
 export const semanticVersionRegex =
   /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
-
-export const authorRegex =
-  /^([^<(]+?)(?:\s*<([^>]+)>)?(?:\s*\(([^)]+)\))?$/;
